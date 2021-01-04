d66415a58d9c15b7a1beebecbe6df433f5e0f146
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-05 00:13:44 +0600

    Added some object

---
+++ objects.js
@@ -0,0 +1,32 @@
+//=====Objects===== 
+
+var person = {};
+person.name = 'Muhammad Shahnewaz';
+person.age = '60';
+person.print = function(){
+    console.log(this.name + ' ' + this.age);
+}; 
+
+person.print();
+//end
+
+
+var person = {
+    name: 'Muhammad Shahnewaz',
+    age: 23,
+    skills: ['HTMl, CSS, JavaScript, React, Node, Bootstrap'],
+
+    print: function(){
+        console.log(this.name, this.age, this.skills);
+    }
+};
+//person name change
+person.name = 'Shahnewaz Khondokar';
+
+person.print();
+console.log(person,['name']);
+
+for (var i in person) {
+    console.log(person[i]);
+    
+}
